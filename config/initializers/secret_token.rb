e704baba987cde3ae8fdce986c8628dfa0888c76
Ryan Dalke <rdalke@gmail.com>
2011-03-11 09:20:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ambire::Application.config.secret_token = '1772a1a3518eb46c401d0d3fee1c567f18fa9667c09bc89a882a3e5d76acadc3981645ea6392f349b3f5d773eee8150eafe36eb45a7b2c09b787c91548fa86e6'
